16106e08c0455de05c8b4849a4d33a5fffe4a1f9
fstrauf <f.strauf@gmail.com>
2022-11-17 15:12:52 +1100

    creates back in

--- pages/api/post/insertNotionContent.ts
+++ pages/api/post/insertNotionContent.ts
@@ -16,8 +16,6 @@ export default async function handler(
 
   const { notionResult } = req.body;
 
-  // console.log(notionResult)
-
   // const notion = new Client({
   //   // auth: 'secret_tPIpoJbE56kTSUokH2i7sDro4OK5yAq7TkeKc8gdswY',
   //   auth: process.env.NOTION_TOKEN
@@ -171,42 +169,42 @@ export default async function handler(
 
   // console.log(contentAuthors)
 
-  // var authorCallsMoreAuthors = []
-  // notionResult.forEach((element, contentIndex) => {
-  //   if (element.users.length > 1) {
-  //     element.users.forEach((user, userIndex) => {
-  //       //skip first
-  //       if(userIndex==0){
-  //         return
-  //       }
-  //       authorCallsMoreAuthors.push(
-  //         prisma.contentAuthor.create({
-  //           data: {
-  //             content: {
-  //               connect: {
-  //                 id: contentAuthors[contentIndex].contentId,
-  //               }
-  //             },
-  //             user: {
-  //               connectOrCreate: {
-  //                 where: {
-  //                   email: user.person.email,
-  //                 },
-  //                 create: {
-  //                   email: user.person.email,
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         })
-  //       )
-  //     })
-  //   }
-  // })
+  var authorCallsMoreAuthors = []
+  notionResult.forEach((element, contentIndex) => {
+    if (element.users.length > 1) {
+      element.users.forEach((user, userIndex) => {
+        //skip first
+        if(userIndex==0){
+          return
+        }
+        authorCallsMoreAuthors.push(
+          prisma.contentAuthor.create({
+            data: {
+              content: {
+                connect: {
+                  id: contentAuthors[contentIndex].contentId,
+                }
+              },
+              user: {
+                connectOrCreate: {
+                  where: {
+                    email: user.person.email,
+                  },
+                  create: {
+                    email: user.person.email,
+                  }
+                }
+              }
+            }
+          })
+        )
+      })
+    }
+  })
 
-  // const contentMoreAuthors = await prisma.$transaction(
-  //   authorCallsMoreAuthors
-  // )
+  const contentMoreAuthors = await prisma.$transaction(
+    authorCallsMoreAuthors
+  )
 
   // console.log(contentMoreAuthors)
 
