36d8924b01821dadb479f044bcc5071ecc8ab2b0
fstrauf <f.strauf@gmail.com>
2022-11-21 19:21:44 +1100

    slate text bnox
    slate text box

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,14 +1,11 @@
-// pages/p/[id].tsx
-
-import React from 'react';
-import { GetServerSideProps } from 'next';
-// import ReactMarkdown from 'react-markdown';
-import Router from 'next/router';
-// import Layout from '../../components/Layout';
-import Layout from '../../components/Layout';
-import { PostProps } from '../../components/Post';
-import { useSession } from 'next-auth/react';
-import prisma from '../../lib/prisma';
+import React from "react";
+import { GetServerSideProps } from "next";
+import ReactMarkdown from "react-markdown";
+import Layout from "../../components/Layout";
+import Router from "next/router";
+import { PostProps } from "../../components/Post";
+import { useSession } from "next-auth/react";
+import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -28,22 +25,21 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 async function publishPost(id: string): Promise<void> {
   await fetch(`/api/publish/${id}`, {
-    method: 'PUT',
+    method: "PUT",
   });
-  await Router.push('/');
+  await Router.push("/");
 }
 
 async function deletePost(id: string): Promise<void> {
   await fetch(`/api/post/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
-  Router.push('/');
+  Router.push("/");
 }
 
-
 const Post: React.FC<PostProps> = (props) => {
-  const { data: session, status } = useSession();
-  if (status === 'loading') {
+  const {data: session, status} = useSession();
+  if (status === "loading") {
     return <div>Authenticating ...</div>;
   }
   const userHasValidSession = Boolean(session);
@@ -57,19 +53,117 @@ const Post: React.FC<PostProps> = (props) => {
     <Layout>
       <div>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || 'Unknown author'}</p>
-        {/* <ReactMarkdown children={props.content} /> */}
+        <p>By {props?.author?.name || "Unknown author"}</p>
+        <ReactMarkdown children={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
         )}
-        {
-          userHasValidSession && postBelongsToUser && (
-            <button onClick={() => deletePost(props.id)}>Delete</button>
-          )
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
+        )}
+      </div>
+      <style jsx>{`
+        .page {
+          background: white;
+          padding: 2rem;
         }
-      </div>     
+
+        .actions {
+          margin-top: 2rem;
+        }
+
+        button {
+          background: #ececec;
+          border: 0;
+          border-radius: 0.125rem;
+          padding: 1rem 2rem;
+        }
+
+        button + button {
+          margin-left: 1rem;
+        }
+      `}</style>
     </Layout>
   );
 };
 
 export default Post;
+
+
+
+
+// // pages/p/[id].tsx
+
+// import React from 'react';
+// import { GetServerSideProps } from 'next';
+// // import ReactMarkdown from 'react-markdown';
+// import Router from 'next/router';
+// // import Layout from '../../components/Layout';
+// import Layout from '../../components/Layout';
+// import { PostProps } from '../../components/Post';
+// import { useSession } from 'next-auth/react';
+// import prisma from '../../lib/prisma';
+
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const post = await prisma.post.findUnique({
+//     where: {
+//       id: String(params?.id),
+//     },
+//     include: {
+//       author: {
+//         select: { name: true, email: true },
+//       },
+//     },
+//   });
+//   return {
+//     props: post,
+//   };
+// };
+
+// async function publishPost(id: string): Promise<void> {
+//   await fetch(`/api/publish/${id}`, {
+//     method: 'PUT',
+//   });
+//   await Router.push('/');
+// }
+
+// async function deletePost(id: string): Promise<void> {
+//   await fetch(`/api/post/${id}`, {
+//     method: 'DELETE',
+//   });
+//   Router.push('/');
+// }
+
+
+// const Post: React.FC<PostProps> = (props) => {
+//   const { data: session, status } = useSession();
+//   if (status === 'loading') {
+//     return <div>Authenticating ...</div>;
+//   }
+//   const userHasValidSession = Boolean(session);
+//   const postBelongsToUser = session?.user?.email === props.author?.email;
+//   let title = props.title;
+//   if (!props.published) {
+//     title = `${title} (Draft)`;
+//   }
+
+//   return (
+//     <Layout>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>By {props?.author?.name || 'Unknown author'}</p>
+//         {/* <ReactMarkdown children={props.content} /> */}
+//         {!props.published && userHasValidSession && postBelongsToUser && (
+//           <button onClick={() => publishPost(props.id)}>Publish</button>
+//         )}
+//         {
+//           userHasValidSession && postBelongsToUser && (
+//             <button onClick={() => deletePost(props.id)}>Delete</button>
+//           )
+//         }
+//       </div>     
+//     </Layout>
+//   );
+// };
+
+// export default Post;
