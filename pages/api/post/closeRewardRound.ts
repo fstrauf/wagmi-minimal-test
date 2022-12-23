d5f411a6f8b026739e2167033e46129757d4a162
fstrauf <f.strauf@gmail.com>
2022-12-23 17:06:53 +1100

    voting fixed

--- pages/api/post/closeRewardRound.ts
+++ pages/api/post/closeRewardRound.ts
@@ -5,15 +5,14 @@ export default async function handle(req, res) {
   const contentPointsVoted = rewardRound.Content.reduce((a, v) => a = a + Number(v.pointsVote), 0)
   var authorCash:number = 0
 
+  //Content Reward Payout
   var payout = []
   rewardRound.Content.forEach(content => {
     content.ContentAuthor.forEach(contentAuthor => {
       authorCash = content.pointsVote / contentPointsVoted * rewardRound.budget / content.ContentAuthor.length
       if(isNaN(authorCash)){ //probably a 0 somewhere in division
         authorCash = 0
-      }
-      
-      // console.log(authorCash)
+      }    
       payout.push({
         userId: contentAuthor.userId,
         authorCash: authorCash,
@@ -26,14 +25,43 @@ export default async function handle(req, res) {
   var teamCash:number = 0
   var ownership:number = 0
 
+  const memberVote = await prisma.memberVote.groupBy({
+    by: ['teamValueAddId'],
+    _sum: {
+      pointsSpent: true,
+    },
+  })
+
+  // console.log(groupUsers)
+
+  // [
+  //   {
+  //     _sum: { pointsSpent: 100 },
+  //     teamValueAddId: 'clc00n1oh00apemuf16xldcz1'
+  //   },
+  //   {
+  //     _sum: { pointsSpent: 100 },
+  //     teamValueAddId: 'clc00n1oh00atemufddgwgrsy'
+  //   }
+  // ]
+
+  //Team Reward Payout
   rewardRound.TeamValueAdd.forEach(teamValue => {
     teamValue.RewardRoundTeamMember.forEach(teamMember => {
 
-      teamCash = teamMember.allocationPoints/(Number(teamValue._count.RewardRoundTeamMember)*100)*teamValue.cashAllocation
+      // console.log("team allocation " +teamValue.TeamProposal[0]?.allocation)
+      // console.log("allocation points " +teamMember.allocationPoints)
+      // console.log("membercount " +(Number(teamValue._count.RewardRoundTeamMember)*100))
+
+      var totalTeamSpent = memberVote.find(mv => mv.teamValueAddId === teamValue.id)
+
+      //we always have a total of 100 points to give - should divide these by the number of voters
+      teamCash = teamMember.allocationPoints/Number(totalTeamSpent._sum.pointsSpent)*teamValue.cashAllocation
       if(isNaN(teamCash)){ //probably a 0 somewhere in division
         teamCash = 0
       }
-      ownership = teamValue.TeamProposal[0]?.allocation*(teamMember.allocationPoints/(Number(teamValue._count.RewardRoundTeamMember)*100))
+      //i need to know total points voted.
+      ownership = teamValue.TeamProposal[0]?.allocation*(teamMember.allocationPoints/Number(totalTeamSpent._sum.pointsSpent))
       if(isNaN(ownership)){ //probably a 0 somewhere in division
         ownership = 0
       }
@@ -63,7 +91,7 @@ export default async function handle(req, res) {
 
   var payCalls = []
 
-  // console.log(memberPayout)
+  console.log(memberPayout)
 
   memberPayout.forEach(memberPayout => {
     payCalls.push(prisma.payout.create({
